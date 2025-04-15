42fd402fd8ba86d15a901b31fc7a8a9bf2317f1f
Katya TubisChirko <ktubis@login-biomed-01>
2025-04-15 18:02:04 +0200

    =fixed bash scripts

--- delta_tuning/bash_scripts/run_inference.sh
+++ delta_tuning/bash_scripts/run_inference.sh
@@ -1,1 +1,1 @@
-python run_cell_annotation_model.py --max_seq_len=3001 --train=False --model="pretrained_models/retrain_all_2504141132.pth" --test_data="Muraro"
+python run_cell_annotation_model.py --max_seq_len=3001 --model="retrained_models/finetune_decoder_lr_5e-5_2504141504.pth" --test_data="Muraro"
