bfdec6bbb9da92d23643c9f8956826aa1a9c0150
ktubis <ktubis@ethz.ch>
2025-05-13 16:46:38 +0200

    Add arguments names to call so self.model

--- delta_tuning/model_wrappers/cell_annotation.py
+++ delta_tuning/model_wrappers/cell_annotation.py
@@ -267,8 +267,8 @@ class CellAnnotationModelWrapper():
                 src_key_padding_mask = input_gene_ids.eq(self.vocab[self.pad_token])
                 with torch.cuda.amp.autocast(enabled=True):
                     output_dict = self.model(
-                        input_gene_ids,
-                        input_values,
+                        input_ids=input_gene_ids,
+                        inputs_embeds=input_values,
                         src_key_padding_mask=src_key_padding_mask,
                         CLS=True,
                     )
