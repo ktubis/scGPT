cedc6c0b032385028f07388d4320a2a1d0599b72
ktubis <ktubis@ethz.ch>
2025-10-01 12:48:23 +0200

    change var to obs in the previous commit

--- delta_tuning/model_wrappers/model_wrappers.py
+++ delta_tuning/model_wrappers/model_wrappers.py
@@ -547,8 +547,8 @@ class ScGPTModelWrapper(ABC):
                 avg_attn_adata.obsm[f"transformer_layer_{i}"] = avg_attn_maps[i]
                 cell_attn_adata.obsm[f"transformer_layer_{i}"] = cell_attn_maps[i]
             for adata in [avg_attn_adata, cell_attn_adata]:
-                if "highly_variable_intersection" in adata.var:
-                    adata.var.drop(columns=["highly_variable_intersection"],
+                if "highly_variable_intersection" in adata.obs:
+                    adata.obs.drop(columns=["highly_variable_intersection"],
                                    inplace=True)
             avg_attn_file = attention_maps_file + "_avg.h5ad"
             cell_attn_file = attention_maps_file + "_cell.h5ad"
